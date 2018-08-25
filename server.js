261cb6b6270ff931415ab98d212dcf36e10d4216
jag <jagnoor@gmail.com>
2018-08-25 17:18:16 -0500

    localhost added

--- server.js
+++ server.js
@@ -36,6 +36,6 @@ require("./routes/htmlRoutes")(app);
 // =============================================================
 db.sequelize.sync({}).then(function () {
   app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on localhost:" + PORT);
   });
 });
